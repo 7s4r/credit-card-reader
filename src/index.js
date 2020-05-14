7c5557b8394494d396cac857aaede0ca4ae284ac
Gueorgui AGAPOV <g.agapov@gmail.com>
2020-05-14 17:27:47 +0200

    Add async

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import Device from './Device.js'
 
 const device = new Device()
 
-device.on('card-connected', () => {
+device.on('card-connected', async () => {
   const card = device.getCard()
 
   if (card) {
-    const response = card.selectFile('A0000000031010')
+    const response = await card.selectFile('A0000000031010')
   
     if (response) {
       console.info('Data received', response)
